0605f3f36145d17bdb8504a6c947e90c778e716d
Kirk Sudduth <kirksudduth@gmail.com>
2020-06-08 11:14:24 -0500

    initial commit

---
+++ src/Kandy.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Kandy.css";
+
+function Kandy() {
+  return (
+    <div className="Kandy">
+      <header className="Kandy-header">
+        <p>
+          Edit <code>src/Kandy.js</code> and save to reload.
+        </p>
+        <a
+          className="Kandy-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Kandy;
